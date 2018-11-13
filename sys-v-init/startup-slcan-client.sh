515dbb35c9fe7e2c7eee48707dcb7d9bfa87bc70
David R. Piegdon <dgit@piegdon.de>
2018-11-13 15:28:08 +0000

    expect sys-v-init startup scripts in the same dir

--- sys-v-init/startup-slcan-client.sh
+++ sys-v-init/startup-slcan-client.sh
@@ -4,5 +4,9 @@ CONNECTOR=connectify
 HOST="192.168.23.17"
 PORT="6160"
 
-. $(dirname $0)/startup-slcan-server.sh
+if [ -h "$0" ]; then
+	. $(dirname $(readlink $0))/startup-slcan-server.sh
+else
+	. $(dirname $0)/startup-slcan-server.sh
+fi
 
